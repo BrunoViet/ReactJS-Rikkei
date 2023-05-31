f514ed8fbb3f5f5236c18909a31201e8afd65a0c
BrunoViet <123159709+BrunoViet@users.noreply.github.com>
2023-05-31 20:35:16 +0700

    change state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ class App extends React.Component {
     this.state={
       studentName:'Phuong'
     }
+    // this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(){
+    this.setState({
+      studentName:this.state.studentName=='Sang'?'Phuong':'Sang'
+    })
+   
   }
   render(){
     return(
       <div>
         Student Name: {this.state.studentName}
+        <button onClick={()=>this.handleChange()}>Change Student Name</button>
       </div>
     )
   }
