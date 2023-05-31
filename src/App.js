ff7a1e4269fa551a1692f308dcc5025d06677c0c
BrunoViet <123159709+BrunoViet@users.noreply.github.com>
2023-05-31 19:38:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from "react";
 
 class App extends React.Component {
+  // Cau2
+//   sum=(a,b)=>{
+//     return a+b
+//   }
+//   render() {
 
-  sum=(a,b)=>{
-    return a+b
+//     const number1=6;
+//     const number2=8;
+//   return(
+//     <div><h1>Tong cua {number1} va {number2} la: {this.sum(number1,number2)}</h1></div>
+//   )
+//   }
+
+// Cau3
+
+constructor(props) {
+  super(props);
+  this.state = {
+    time:new Date(),
   }
-  render() {
-
-    const number1=6;
-    const number2=8;
+}
+render(){
+  let time =new Date();
   return(
-    <div><h1>Tong cua {number1} va {number2} la: {this.sum(number1,number2)}</h1></div>
+    <div>
+      <h1>Dong ho</h1>
+      
+    </div>
   )
-  }
+}
+
 }
 
 
