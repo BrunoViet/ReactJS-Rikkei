b9e116ea049d480101159086645bca4828fa5f6e
BrunoViet <123159709+BrunoViet@users.noreply.github.com>
2023-06-02 02:10:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,62 @@
-import React from "react";
+// import React, { useState } from 'react';
+import React from 'react';
+import './App.css';
 
+// function App() {
+//   const [result, setResult] = useState(null);
+
+//   const flipCoin = () => {
+//     const randomResult = Math.random() < 0.5 ? 'Heads' : 'Tails';
+//     setResult(randomResult);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Let's flip a coin!</h1>
+//       <button onClick={flipCoin}>Flip</button>
+//       {result && <h2>{result}</h2>}
+//     </div>
+//   );
+// }
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-     color:"black",
-     isShown: false,
-     fontSize: 10,
-     content:"Hello Babe!"
+    this.state={
+      result:'',
+      countSap:0,
+      countNgua:0,
+      imgSrc:"",
     }
   }
 
-  handleToggle=() => {
-    this.setState({
-      isShown: !this.state.isShown  
-    })
+  handleCoin=() => {
+    let result  = Math.round(Math.random()*10)
+    if(result<5&& result>0) {
+      this.setState({
+        countSap:this.state.countSap+1,
+        imgSrc:'https://www.pcgs.com/UserImages/04436864o.jpg'
+      })
+    }
+    if(result>=5){
+      this.setState({
+        countNgua:this.state.countNgua+1,
+        imgSrc:'https://www.pcgs.com/UserImages/04436864r.jpg'
+      })
   }
+}
 
-  handleChangeBackground=() => {
-    this.setState({
-     color:this.state.color=='black'?'red':'black',
-     fontSize:this.state.fontSize==10?14:10,
-     content:this.state.content=='Hello Babe!'?"I LOVE U":'Hello Babe!'
-    })
-  }
   render() {
     return(
-      <>
-      {this.state.isShown?(
-        <>
-        <p style={{color:this.state.color,fontSize:this.state.fontSize}}>
-        {this.state.content}
+      <div style={{textAlign:'center'}}>
+        <h1>Ấn nút để tung đồng xu</h1>
+        <img src={this.state.imgSrc}/><br/>
+        <button style={{fontSize:"100px"}} onClick={this.handleCoin}>Tung đồng xu</button>
+        <p style={{fontSize:'50px'}}>
+          Sấp:{this.state.countSap} <span> Ngửa:{this.state.countNgua}</span>
         </p>
-        <button onClick={this.handleChangeBackground}>Change</button>
-        </> 
-      ):(
-        ""
-      )}
-      <button onClick={this.handleToggle}>Toggle</button>
-      </>
+        
+      </div>
     )
   }
 }
-export default App
+export default App;
