e7ae1f7ade89871a83685adf139023e56973ca17
BrunoViet <123159709+BrunoViet@users.noreply.github.com>
2023-06-05 20:48:43 +0700

    hookjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-import Login from "./component/Login";
+function App() {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-class App extends React.Component {
+  useEffect(function () {
+    document.title = `You clicked ${count2} times`
+    console.log('hello')
+  }, [count2])
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      age: 26
-    }
+  const handleIncrease = () => {
+    setCount(count + 1)
+    setCount2(count2 + 2)
   }
-  render() {
-    return (
-      <div>
-        <Login />
-      </div>
-    )
-  }
-
+  return (
+    <div>
+      <h1>
+        You clicked {count} times
+        Computer get {count2}
+      </h1>
+      <button onClick={handleIncrease}>
+        Increase
+      </button>
+    </div>
+  )
 }
 export default App
