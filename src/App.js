427d8b2361357f7d1e2ee48121f83bf004f6a60a
BrunoViet <123159709+BrunoViet@users.noreply.github.com>
2023-06-07 19:28:33 +0700

    UI Framework

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import React from "react";
+import { Button as Btn } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends React.Component {
-  // Cau2
-//   sum=(a,b)=>{
-//     return a+b
-//   }
-//   render() {
-
-//     const number1=6;
-//     const number2=8;
-//   return(
-//     <div><h1>Tong cua {number1} va {number2} la: {this.sum(number1,number2)}</h1></div>
-//   )
-//   }
-
-// Cau3
-
-constructor(props) {
-  super(props);
-  this.state = {
-    time:new Date(),
-  }
-}
-render(){
-  let time =new Date();
-  return(
+function App() {
+  return (
     <div>
-      <h1>Dong ho</h1>
-      
+      <Btn>Hello</Btn>
     </div>
   )
 }
-
-}
-
-
 export default App;
