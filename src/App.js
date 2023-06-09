55762c1938bf785d94b43c1ee7bb84b6821dab91
BrunoViet <123159709+BrunoViet@users.noreply.github.com>
2023-06-09 18:56:32 +0700

    Hookjs-TodoList

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
-import { Alert, Space, Spin } from 'antd';
-import './App.css'
-const App = () => (
-  <Space
-    direction="vertical"
-    style={{
-      width: '100%',
-    }}
-  >
-    <Space>
-      <Spin tip="Loading" size="small">
-        <div className="content" />
-      </Spin>
-      <Spin tip="Loading">
-        <div className="content" />
-      </Spin>
-      <Spin tip="Loading" size="large">
-        <div className="content" />
-      </Spin>
-    </Space>
+import React, { useState } from "react";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterForm";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
-    <Spin tip="Loading...">
-      <Alert
-        message="Alert message title"
-        description="Further details about the context of this alert."
-        type="info"
-      />
-    </Spin>
-  </Space>
-);
+function App(props) {
+
+  const [tasks, setTasks] = useState(props.task);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const editTask = (id, newName) => {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
 export default App;
